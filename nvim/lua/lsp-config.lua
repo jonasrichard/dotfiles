0bac6c35feff01973310f4aac1cbaa6187e08dcf
Richard <richard.jonas.76@gmail.com>
2023-03-14 12:16:02 +0100

    Map keys when lsp attached

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -13,3 +13,16 @@ lspconfig.gopls.setup({
 	},
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        vim.keymap.set('n', '<Leader>a', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<Leader>d', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<Leader>e', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, opts)
+        vim.keymap.set('n', '<Leader>i', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<Leader>m', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<Leader>r', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<Leader>q', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<Leader>t', vim.lsp.buf.type_definition, opts)
+    end
+})
--- nvim/lua/rust-config.lua
+++ nvim/lua/rust-config.lua
@@ -1,16 +1,5 @@
 local function on_attach(client, buffer)
 	local opts = {buffer = buffer, remap = false}
-
-	-- LSP key maps
-	vim.keymap.set('n', '<Leader>a', vim.lsp.buf.code_action, opts)
-	vim.keymap.set('n', '<Leader>d', vim.lsp.buf.definition, opts)
-	vim.keymap.set('n', '<Leader>e', vim.lsp.buf.hover, opts)
-	vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, opts)
-	vim.keymap.set('n', '<Leader>i', vim.lsp.buf.implementation, opts)
-	vim.keymap.set('n', '<Leader>m', vim.lsp.buf.rename, opts)
-	vim.keymap.set('n', '<Leader>r', vim.lsp.buf.references, opts)
-	vim.keymap.set('n', '<Leader>q', vim.lsp.buf.hover, opts)
-	vim.keymap.set('n', '<Leader>t', vim.lsp.buf.type_definition, opts)
 end
 
 -- Configure LSP through rust-tools.nvim plugin.
