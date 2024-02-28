ae9ce36218506f6db29017fc4999ce46254c0371
Richard Jonas <richard.jonas.76@gmail.com>
2024-02-28 20:31:36 +0100

    Change quotes

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -13,70 +13,73 @@ end
 -- This line is needed for installing Packer
 local packer_bootstrap = ensure_packer()
 
-require("packer").startup(function(use)
+require('packer').startup(function(use)
     -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+    use('wbthomason/packer.nvim')
     -- Collection of common configurations for the Nvim LSP client
-    use("neovim/nvim-lspconfig")
+    use('neovim/nvim-lspconfig')
 
     -- Treesitter plugin
     use({
-        "nvim-treesitter/nvim-treesitter", run = ":TSUpdate"
+        'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'
     })
 
     -- Visualize lsp progress
     use({
-        "j-hui/fidget.nvim",
-        tag = "legacy",
+        'j-hui/fidget.nvim',
+        tag = 'legacy',
         config = function()
-            require("fidget").setup()
+            require('fidget').setup()
         end
     })
 
     -- Autocompletion framework
-    use("hrsh7th/nvim-cmp")
+    use('hrsh7th/nvim-cmp')
     use({
         -- cmp LSP completion
-        "hrsh7th/cmp-nvim-lsp",
+        'hrsh7th/cmp-nvim-lsp',
         -- cmp Snippet completion
-        "hrsh7th/cmp-vsnip",
+        'hrsh7th/cmp-vsnip',
         -- cmp Path completion
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-buffer",
-        after = { "hrsh7th/nvim-cmp" },
-        requires = { "hrsh7th/nvim-cmp" },
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-buffer',
+        after = { 'hrsh7th/nvim-cmp' },
+        requires = { 'hrsh7th/nvim-cmp' },
     })
     -- See hrsh7th other plugins for more great completion sources!
     -- Snippet engine
     use('hrsh7th/vim-vsnip')
 
     -- Adds extra functionality over rust analyzer
-    use("mrcjkb/rustaceanvim")
+    use('mrcjkb/rustaceanvim')
 
     -- Elixir
-    use("elixir-tools/elixir-tools.nvim")
+    use('elixir-tools/elixir-tools.nvim')
 
     -- Optional
-    use("nvim-lua/popup.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("nvim-telescope/telescope.nvim")
+    use('nvim-lua/popup.nvim')
+    use('nvim-lua/plenary.nvim')
+    use('nvim-telescope/telescope.nvim')
 
     -- Some color scheme other then default
-    use("morhetz/gruvbox")
+    use('morhetz/gruvbox')
 
     -- Git plugin
-    use("tpope/vim-fugitive")
+    use('tpope/vim-fugitive')
 
-    use("nvim-tree/nvim-tree.lua")
-    use("nvim-tree/nvim-web-devicons")
-
-    use("nvim-lualine/lualine.nvim")
-
-    use("windwp/nvim-ts-autotag")
-
+    -- Tree and tab line
+    use('nvim-tree/nvim-tree.lua')
+    use('nvim-tree/nvim-web-devicons')
+    use('nvim-lualine/lualine.nvim')
     -- https://neovimcraft.com/plugin/akinsho/bufferline.nvim
     use({ 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' })
 
+    -- Auto close tags
+    use('windwp/nvim-ts-autotag')
+
+    use('sindrets/diffview.nvim')
+
+    -- Diffing directories
     use('will133/vim-dirdiff')
 end)
 
