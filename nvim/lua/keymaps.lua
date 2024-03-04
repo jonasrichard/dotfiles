42c7555c241c02f9d55d8231eac4c6705363f445
Richard Jonas <richard.jonas.76@gmail.com>
2024-03-04 21:16:45 +0100

    Move window borders according to window position

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -20,10 +20,10 @@ vim.keymap.set('n', '<Leader>k', '<c-w>k')
 vim.keymap.set('n', '<Leader>l', '<c-w>l')
 
 -- Resize windows
-vim.keymap.set('n', '<up>', '<Cmd>resize -5<CR>')
-vim.keymap.set('n', '<down>', '<Cmd>resize +5<CR>')
-vim.keymap.set('n', '<left>', '<Cmd>vertical resize +5<CR>')
-vim.keymap.set('n', '<right>', '<Cmd>vertical resize -5<CR>')
+vim.keymap.set('n', '<up>', function() resize_current_window(0, -5) end)
+vim.keymap.set('n', '<down>', function() resize_current_window(0, 5) end)
+vim.keymap.set('n', '<left>', function() resize_current_window(-5, 0) end)
+vim.keymap.set('n', '<right>', function() resize_current_window(5, 0) end)
 
 vim.keymap.set('v', '<Leader>y', '"*y')
 -- Replace without spoiling the yank buffer
@@ -32,3 +32,41 @@ vim.keymap.set('n', '<Leader>yl', function() vim.fn.setreg('*', vim.fn.getline('
 
 vim.keymap.set('v', '<Leader>ue', '!python3 -c "import sys; from urllib import parse; print(parse.quote_plus(sys.stdin.read().strip()))"<cr>')
 vim.keymap.set('v', '<Leader>ud', '!python3 -c "import sys; from urllib import parse; print(parse.unquote_plus(sys.stdin.read().strip()))"<cr>')
+
+function resize_current_window(hplus, vplus)
+    local win_id = vim.fn.win_getid()
+    local pos = vim.api.nvim_win_get_position(win_id)
+
+    if hplus ~= 0 then
+        -- horizontal sizing
+        local current_width = vim.api.nvim_win_get_width(win_id)
+
+        -- is it not the leftmost window?
+        if pos[2] > 1 then
+            hplus = hplus * -1
+        end
+
+        local new_width = current_width + hplus
+
+        if new_width < 5 then
+            new_width = current_width
+        end
+
+        vim.api.nvim_win_set_width(win_id, new_width)
+    elseif vplus ~= 0 then
+        -- vertical sizing
+        local current_height = vim.api.nvim_win_get_height(win_id)
+
+        if pos[1] > 1 then
+            vplus = vplus * -1
+        end
+
+        local new_height = current_height + vplus
+
+        if new_height < 3 then
+            new_height = current_height
+        end
+
+        vim.api.nvim_win_set_height(win_id, new_height)
+    end
+end
