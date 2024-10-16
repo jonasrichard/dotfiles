c22d0097f8ccc6a8beb3dc3592eba5bf2d91a9c9
Richard Jonas <richard.jonas.76@gmail.com>
2024-10-16 20:29:50 +0200

    Add nvim lazy config

---
+++ nvim-lazy/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- Keymap are automatically loaded on the VeryLazy event
+-- Default keymap that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<f4>", "tabnext", { desc = "Next buffer" })
